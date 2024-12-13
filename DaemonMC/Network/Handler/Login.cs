43af2c60fae3795025a908065aecd0c57229fdc2
laz1444 <robertsrihards@inbox.lv>
2024-12-13 14:01:58 +0200

    better JWT processing

--- DaemonMC/Network/Handler/Login.cs
+++ DaemonMC/Network/Handler/Login.cs
@@ -1,6 +1,7 @@
 ﻿using DaemonMC.Network.Bedrock;
 using DaemonMC.Utils;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace DaemonMC.Network.Handler
 {
@@ -10,13 +11,12 @@ namespace DaemonMC.Network.Handler
         {
             byte[] jwtBuffer = Encoding.UTF8.GetBytes(packet.request);
 
-            var filteredJWT = new string(packet.request.Where(c => c >= 32 && c <= 126).ToArray());
-            int jsonEndIndex = filteredJWT.LastIndexOf('}');
-            if (jsonEndIndex >= 0)
-            {
-                filteredJWT = filteredJWT.Substring(0, jsonEndIndex + 1);
-            }
-            string Token = Encoding.UTF8.GetString(jwtBuffer, filteredJWT.Length + 8, jwtBuffer.Length - (filteredJWT.Length + 8));
+            string pattern = @"{""chain"":\[.*?\]}";
+            var match = Regex.Match(packet.request, pattern);
+
+            var filteredJWT = match.Value;
+            int tokenStartIndex = Encoding.UTF8.GetBytes(filteredJWT).Length + 8;
+            var Token = Encoding.UTF8.GetString(jwtBuffer, tokenStartIndex, jwtBuffer.Length - tokenStartIndex);
 
             JWT.processJWTchain(filteredJWT);
             JWT.processJWTtoken(Token);

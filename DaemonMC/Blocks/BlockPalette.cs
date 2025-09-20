401fa971ae44c49a6ed39519cda8aa4f8566e5e5
laz1444 <robertsrihards@inbox.lv>
2025-09-20 11:28:34 +0300

    fix missing block states

--- DaemonMC/Blocks/BlockPalette.cs
+++ DaemonMC/Blocks/BlockPalette.cs
@@ -16,10 +16,60 @@ namespace DaemonMC.Blocks
             {
                 Block blockInstance = (Block)Activator.CreateInstance(type);
 
-                int hash = blockInstance.GetHash();
+                var stateOptions = new Dictionary<string, object[]>();
+                foreach (var state in blockInstance.States)
+                {
+                    if (BlockStates.States.TryGetValue(state.Key, out var possibleValues))
+                    {
+                        stateOptions[state.Key] = possibleValues;
+                    }
+                    else
+                    {
+                        stateOptions[state.Key] = new object[] { state.Value };
+                    }
+                }
 
-                blockHashes[hash] = blockInstance;
+                foreach (var combination in GetCombinations(stateOptions))
+                {
+                    Block newBlock = (Block)Activator.CreateInstance(type);
+
+                    foreach (var kvp in combination)
+                    {
+                        newBlock.States[kvp.Key] = kvp.Value;
+                    }
+
+                    int hash = newBlock.GetHash();
+                    blockHashes[hash] = newBlock;
+                }
             }
+        }
+
+
+        private static IEnumerable<Dictionary<string, object>> GetCombinations(Dictionary<string, object[]> options)
+        {
+            var keys = options.Keys.ToList();
+
+            IEnumerable<Dictionary<string, object>> Recurse(int index)
+            {
+                if (index == keys.Count)
+                {
+                    yield return new Dictionary<string, object>();
+                }
+                else
+                {
+                    string key = keys[index];
+                    foreach (var value in options[key])
+                    {
+                        foreach (var rest in Recurse(index + 1))
+                        {
+                            rest[key] = value;
+                            yield return new Dictionary<string, object>(rest);
+                        }
+                    }
+                }
+            }
+
+            return Recurse(0);
         }
     }
 }
---
+++ Test/Blocks.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Test
+{
+    [TestClass]
+    public class Blocks
+    {
+        [TestMethod]
+        public void GenerateStates()
+        {
+            string json = File.ReadAllText("block_states.json"); //https://github.com/SerenityJS/bedrock-data/blob/main/data/1.21.90/block_states.json
+            var definitions = JsonSerializer.Deserialize<List<BlockStateDefinition>>(json);
+
+            var sb = new StringBuilder();
+
+            sb.AppendLine("public static readonly Dictionary<string, object[]> States = new()");
+            sb.AppendLine("{");
+
+            foreach (var def in definitions)
+            {
+                string valuesString;
+
+                switch (def.Type)
+                {
+                    case "bool":
+                        valuesString = string.Join(", ", def.Values.Select(v => v.GetBoolean() ? "(byte)1" : "(byte)0"));
+                        break;
+
+                    case "int":
+                        valuesString = string.Join(", ", def.Values.Select(v => v.GetInt32().ToString()));
+                        break;
+
+                    case "string":
+                        valuesString = string.Join(", ", def.Values.Select(v => $"\"{v.GetString()}\""));
+                        break;
+
+                    default:
+                        throw new Exception($"unknown type {def.Type}");
+                }
+
+                sb.AppendLine($"    {{ \"{def.Identifier}\", new object[] {{ {valuesString} }} }},");
+            }
+
+            sb.AppendLine("};");
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+
+    public class BlockStateDefinition
+    {
+        [JsonPropertyName("identifier")]
+        public string Identifier { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("values")]
+        public JsonElement[] Values { get; set; }
+    }
+}

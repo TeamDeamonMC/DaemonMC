d80e66b562cc124608c8ab372547cd00276db81d
laz1444 <robertsrihards@inbox.lv>
2025-10-30 12:50:15 +0200

    bump leveldb

--- DaemonMC/Level/World.cs
+++ DaemonMC/Level/World.cs
@@ -17,7 +17,7 @@ namespace DaemonMC.Level
 {
     public class World
     {
-        private string[] LevelDbVersion { get; set; } = ["1.21.110"];
+        private string[] LevelDbVersion { get; set; } = ["1.21.110", "1.21.120"];
         public Dictionary<(int x, int z), Chunk> Cache { get; set; } = new Dictionary<(int x, int z), Chunk>();
         public bool Temporary { get; set; } = false;
         public string LevelName { get; set; } = "";
